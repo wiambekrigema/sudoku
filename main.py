fc6c721bda4b9a9b3fed353ddb1cee5ab6236099
wiambekrigema <wiambekrigema@github.com>
2023-11-17 09:11:39 +0100

    update main

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from sudoku import Sudoku
 def main():
   sudoku = Sudoku(9)
   print(" la grille de jeu")
-
+  print("Entrer les numéros dans la grille. Pour une cellule vide, il faut rentrer 0 .")
+  sudoku.put_number()
+  print("Solving the game...")
+  sudoku.solve()
